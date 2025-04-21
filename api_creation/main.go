6b55ea864c7b273c82d28b6754a9b3cef98423b1
jashby360 <jashby360@gmail.com>
2025-04-21 16:11:56 -0400

    push up api creation changes for testing on personal computer. will need to swap around later after testing templates work.

--- api_creation/main.go
+++ api_creation/main.go
@@ -1,15 +1,55 @@
-package main
+// package main
 
-import (
-	"net/http"
+// import (
+// 	"fmt"
+// 	"html/template"
+// 	"io"
+// 	"net/http"
 
-	"github.com/labstack/echo/v4"
-)
+// 	"github.com/labstack/echo/v4"
+// )
 
-func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
-}
+// type TemplateRenderer struct {
+// 	templates *template.Template
+// }
+
+// // Render renders a template document
+// func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+// 	// Add global methods if data is a map
+// 	if viewContext, isMap := data.(map[string]interface{}); isMap {
+// 		viewContext["reverse"] = c.Echo().Reverse
+// 	}
+
+// 	return t.templates.ExecuteTemplate(w, name, data)
+// }
+
+// func main() {
+// 	e := echo.New()
+// 	renderer := &TemplateRenderer{
+// 		templates: template.Must(template.ParseGlob("templates/*.html")),
+// 	}
+// 	e.Renderer = renderer
+
+// 	e.GET("/", func(c echo.Context) error {
+// 		users := GetUsers()
+
+// 		return c.Render(http.StatusOK, "test.html", users)
+// 		// return c.String(http.StatusOK, "Hello, World!")
+// 	})
+
+// 	e.Logger.Fatal(e.Start(":1323"))
+// 	fmt.Println("Server is running on port 1323")
+// }
+
+// type User struct {
+// 	Name  string
+// 	Email string
+// }
+
+// func GetUsers() []User {
+// 	return []User{
+// 		{Name: "John Doe", Email: "johndoe@example.com"},
+// 		{Name: "Alice Smith", Email: "alicesmith@example.com"},
+// 	}
+// }
