48e81ed01410620131a2fc716953b52e8e401f4c
Jamal <jashby360@gmail.com>
2025-04-22 03:49:31 -0400

    implemented templates, still need to work on nesteed templates

--- api_creation/main.go
+++ api_creation/main.go
@@ -1,55 +1,113 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"html/template"
-// 	"io"
-// 	"net/http"
+import (
+	"html/template"
+	"io"
+	"net/http"
 
-// 	"github.com/labstack/echo/v4"
-// )
+	"github.com/labstack/echo/v4"
+)
 
-// type TemplateRenderer struct {
-// 	templates *template.Template
-// }
+type User struct {
+	Name  string
+	Email string
+}
 
-// // Render renders a template document
-// func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
 
-// 	// Add global methods if data is a map
-// 	if viewContext, isMap := data.(map[string]interface{}); isMap {
-// 		viewContext["reverse"] = c.Echo().Reverse
-// 	}
+var users []User
 
-// 	return t.templates.ExecuteTemplate(w, name, data)
-// }
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 
-// func main() {
-// 	e := echo.New()
-// 	renderer := &TemplateRenderer{
-// 		templates: template.Must(template.ParseGlob("templates/*.html")),
-// 	}
-// 	e.Renderer = renderer
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
 
-// 	e.GET("/", func(c echo.Context) error {
-// 		users := GetUsers()
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
-// 		return c.Render(http.StatusOK, "test.html", users)
-// 		// return c.String(http.StatusOK, "Hello, World!")
-// 	})
+func main() {
+	e := echo.New()
 
-// 	e.Logger.Fatal(e.Start(":1323"))
-// 	fmt.Println("Server is running on port 1323")
-// }
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+	e.Renderer = renderer
 
-// type User struct {
-// 	Name  string
-// 	Email string
-// }
+	// GET ROUTE FIRST VISITING PAGE
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "users.html", map[string]interface{}{
+			"name":  "Dolly!",
+			"email": "dolly@example.com",
+			"users": initialUsers(),
+		})
+	})
 
-// func GetUsers() []User {
-// 	return []User{
-// 		{Name: "John Doe", Email: "johndoe@example.com"},
-// 		{Name: "Alice Smith", Email: "alicesmith@example.com"},
-// 	}
-// }
+	e.POST("/users", func(c echo.Context) error {
+
+		tmpl := template.Must(template.ParseFiles("user_row.html"))
+
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		user := User{
+			Name: name, Email: email,
+		}
+
+		users = append(users, user)
+		// fmt.Println("Users:", users)
+
+		err := tmpl.Execute(c.Response().Writer, user)
+		if err != nil {
+			panic(err)
+		}
+
+		return nil
+	})
+
+	e.DELETE("/users-delete/:name", func(c echo.Context) error {
+		tmpl := template.Must(template.ParseFiles("user_row_delete.html"))
+
+		name := c.Param("name")
+
+		// fmt.Println("Name to delete:", name)
+
+		users = removeObject(users, name)
+
+		user := User{
+			Name: "", Email: "",
+		}
+		err := tmpl.Execute(c.Response().Writer, user)
+		if err != nil {
+			panic(err)
+		}
+
+		return nil
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func removeObject(slice []User, value string) []User {
+	var newSlice []User
+	for _, user := range slice {
+		if user.Name != value {
+			newSlice = append(newSlice, user)
+		}
+	}
+	return newSlice
+}
+
+func initialUsers() []User {
+	users = []User{
+		{Name: "John Doe", Email: "johndoe@example.com"},
+		{Name: "Alice Smith", Email: "alicesmith@example.com"},
+	}
+
+	return users
+}
